be8a9c196d67683f2ecb83bb307535e373168ec3
seidutakizawa <bp420blaze@gmail.com>
2025-02-22 21:57:09 +0200

    v0.1

--- src/main/java/com/hustle/Traineeship/Management/Application/TraineeshipManagementApplication.java
+++ src/main/java/com/hustle/Traineeship/Management/Application/TraineeshipManagementApplication.java
@@ -2,12 +2,21 @@ package com.hustle.Traineeship.Management.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(path = "print")
 public class TraineeshipManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraineeshipManagementApplication.class, args);
-	}
 
+	}
+	@GetMapping
+	public String print(){
+		return "Hello World";
+	}
 }
