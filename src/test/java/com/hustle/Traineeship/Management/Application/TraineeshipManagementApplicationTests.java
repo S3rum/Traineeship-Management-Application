e1cf8a574fe54173782244aa42c79f9e8ceba1c6
kourkakis <kostaskourkakis@gmail.com>
2025-02-26 22:23:55 +0200

    v0.9 +Tests MinorFixes

---
+++ src/test/java/com/hustle/Traineeship/Management/Application/TraineeshipManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hustle.Traineeship.Management.Application;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+class TraineeshipManagementApplicationTests {
+
+}
+
