92f992156b7855dc4289344cd8401f4f81376b91
s3rum <tasospa2001@gmail.com>
2025-03-05 09:51:35 +0200

    v2.1 deleted unused code

--- src/main/java/com/hustle/Traineeship/Management/Application/controllers/AuthController.java
+++ src/main/java/com/hustle/Traineeship/Management/Application/controllers/AuthController.java
@@ -1,14 +1,9 @@
 package com.hustle.Traineeship.Management.Application.controllers;
 
-import com.hustle.Traineeship.Management.Application.config.CustomAuthenticationSuccessHandler;
+
 import com.hustle.Traineeship.Management.Application.model.User;
-import com.hustle.Traineeship.Management.Application.repos.StudentRepository;
-import com.hustle.Traineeship.Management.Application.service.ProfessorService;
-import com.hustle.Traineeship.Management.Application.service.StudentsService;
-import com.hustle.Traineeship.Management.Application.service.CompanyService;
 import com.hustle.Traineeship.Management.Application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired
-    private StudentsService studentsService;
 
     @Autowired
     private UserService userService;
@@ -44,11 +37,6 @@ public class AuthController {
             model.addAttribute("errorMessage", e.getMessage());
             return "register";
         }
-    }
-
-
-    public AuthenticationSuccessHandler customSuccessHandler(StudentsService studentsService, ProfessorService professorService,CompanyService companyService) {
-        return new CustomAuthenticationSuccessHandler(studentsService, professorService,companyService);
     }
 
     // Display the login form using Thymeleaf
